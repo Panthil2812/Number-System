07eb54fd4c58a879bd4dd6095cecec5f4a397d68
panthil malaviya <Toshiba@Panthil_Malaviya>
2020-08-08 20:19:50 +0530

    splash screen and logo inserted

---
+++ app/src/main/java/com/rku/number_system/SplashActivity.java
@@ -0,0 +1,29 @@
+package com.rku.number_system;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.AnimationUtils;
+
+public class SplashActivity extends AppCompatActivity {
+    Handler handler;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        getSupportActionBar().hide();
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, 3*1000);
+    }
+}
